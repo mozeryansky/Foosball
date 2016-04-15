d72584636f04bd361c98dc930fc390c5248e10f1
Michael Ozeryansky <michael@res400d-128-61-103-202.res.gatech.edu>
2016-04-15 06:36:52 -0400

    created Xcode project

---
+++ FoosballController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoosballController
+//
+//  Created by Michael Ozeryansky on 4/15/16.
+//  Copyright © 2016 Michael Ozeryansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoosballController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoosballController
+//
+//  Created by Michael Ozeryansky on 4/15/16.
+//  Copyright © 2016 Michael Ozeryansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
